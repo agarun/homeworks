e566367bb8b80b25e83f671cccbde1decee14ac6
agarun <19801205+agarun@users.noreply.github.com>
2017-11-11 23:22:55 -0500

    Add colorized Simon game

---
+++ homeworks/W2D1/lib/simon_colorize.rb
@@ -0,0 +1,105 @@
+require "colorize"
+
+class Simon
+  COLORS = %w(red blue green yellow)
+
+  attr_accessor :sequence_length, :seq, :game_over
+
+  def initialize
+    @sequence_length = 1
+    @seq = []
+    @game_over = false
+  end
+
+  def play
+    welcome_message
+
+    take_turn until game_over?
+
+    game_over_message
+    reset_game
+    play
+  end
+
+  def take_turn
+    show_sequence
+    require_sequence
+
+    round_success_message
+    self.sequence_length += 1
+  end
+
+  def show_sequence
+    add_random_color
+    sleep(2)
+
+    seq.each do |color|
+      system("clear") || system("cls")
+      puts color.colorize(color.to_sym)
+      sleep(1)
+
+      system("clear") || system("cls")
+      puts color.colorize(:color => color.to_sym, :background => color.to_sym)
+      sleep(0.1)
+    end
+  end
+
+  def require_sequence
+    system("clear") || system("cls")
+    puts "What was the sequence?\nType each color's first letter individually:"
+
+    seq.each do |correct_color|
+      begin
+        color_choice = gets.chomp
+        if color_choice.length > 1
+          raise
+        elsif color_choice != correct_color[0]
+          @game_over = true
+          break
+        end
+      rescue
+        puts "Try entering a valid color.\nPick from: #{colors_message(COLORS)}"
+        retry
+      end
+    end
+  end
+
+  def add_random_color
+    @seq << COLORS.sample
+  end
+
+  def game_over?
+    @game_over
+  end
+
+  def welcome_message
+    system("clear") || system("cls")
+    puts "Let's play Simon.\nYou'll see some colors in some order.\nRemember the order!"
+  end
+
+  def round_success_message
+    puts "Round #{sequence_length} is over."
+  end
+
+  def game_over_message
+    puts "Game over. You lasted #{sequence_length - 1} rounds.\nYou meant to write: #{colors_message(seq)}"
+  end
+
+  def colors_message(sequence)
+    sequence.map { |clr| clr[0].colorize(clr.to_sym) }.join(", ")
+  end
+
+  def reset_game
+    reset_message
+    @sequence_length = 1
+    @seq = []
+    @game_over = false
+  end
+
+  def reset_message
+    puts "Game was reset.".colorize(seq.last.to_sym).underline
+    sleep(3)
+  end
+end
+
+Simon.new.play if $PROGRAM_NAME == __FILE__
