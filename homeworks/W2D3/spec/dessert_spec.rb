2a9e2ac8165cf1f7cdfdbe63e5818966fe2f8046
agarun <19801205+agarun@users.noreply.github.com>
2017-11-14 00:02:16 -0500

    Add specs

--- homeworks/W2D3/spec/dessert_spec.rb
+++ homeworks/W2D3/spec/dessert_spec.rb
@@ -1,42 +1,65 @@
 require 'rspec'
 require 'dessert'
 
-=begin
-Instructions: implement all of the pending specs (the `it` statements without blocks)! Be sure to look over the solutions when you're done.
-=end
-
 describe Dessert do
+  subject(:tiramisu) { Dessert.new("custard", 10, "batali") }
   let(:chef) { double("chef") }
 
   describe "#initialize" do
-    it "sets a type"
+    it "sets a type" do
+      expect(tiramisu.type).to eq("custard")
+    end
 
-    it "sets a quantity"
+    it "sets a quantity" do
+      expect(tiramisu.quantity).to_not be_falsy
+    end
 
-    it "starts ingredients as an empty array"
+    it "starts ingredients as an empty array" do
+      expect(tiramisu.ingredients).to be_empty
+    end
 
-    it "raises an argument error when given a non-integer quantity"
+    it "raises an argument error when given a non-integer quantity" do
+      expect { Dessert.new("", "test", "") }.to raise_error(ArgumentError)
+    end
   end
 
   describe "#add_ingredient" do
-    it "adds an ingredient to the ingredients array"
+    it "adds an ingredient to the ingredients array" do
+      tiramisu.add_ingredient("sugar")
+      expect(tiramisu.ingredients).to include("sugar")
+    end
   end
 
   describe "#mix!" do
-    it "shuffles the ingredient array"
+    let(:pre_shuffle) { double(tiramisu.ingredients) }
+
+    it "shuffles the ingredient array" do
+      tiramisu.mix!
+      expect(tiramisu.ingredients).to_not eq(:pre_shuffle)
+    end
   end
 
   describe "#eat" do
-    it "subtracts an amount from the quantity"
+    it "subtracts an amount from the quantity" do
+      tiramisu.eat(1)
+      expect(tiramisu.quantity).to eq(9)
+    end
 
-    it "raises an error if the amount is greater than the quantity"
+    it "raises an error if the amount is greater than the quantity" do
+      expect { tiramisu.eat(11) }.to raise_error
+    end
   end
 
   describe "#serve" do
-    it "contains the titleized version of the chef's name"
+    it "contains the titleized version of the chef's name" do
+      expect(tiramisu.serve).to include("Batali")
+    end
   end
 
   describe "#make_more" do
-    it "calls bake on the dessert's chef with the dessert passed in"
+    it "calls bake on the dessert's chef with the dessert passed in" do
+      expect(chef).to receive(:bake).with(tiramisu)
+      chef.bake(tiramisu)
+    end
   end
 end
