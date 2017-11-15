c0e985d9fc04478fa9a72b47ab257cc576b7bd4e
agarun <19801205+agarun@users.noreply.github.com>
2017-11-14 20:53:10 -0500

    Refactor Board instance methods

--- classwork/W2D2/chess/board.rb
+++ classwork/W2D2/chess/board.rb
@@ -3,22 +3,7 @@ require_relative "piece"
 class Board
   attr_accessor :grid
 
-  def dup
-    new_board = Board.new
-
-    self.grid.each_with_index do |row, i|
-      row.each_with_index do |piece, j|
-        if piece.is_a?(NullPiece)
-          new_board[[i,j]] = NullPiece.instance
-        else
-          new_board[[i,j]] = piece.class.new(piece.position, new_board, piece.color)
-        end
-      end
-    end
-    new_board
-  end
-
-  def initialize()
+  def initialize
     @grid = self.make_starting_grid
   end
 
@@ -27,23 +12,15 @@ class Board
     grid[row][col]
   end
 
-  def get_pieces(color)
-    @grid.flatten.select{|piece| piece.color == color}
-  end
-
   def []=(pos, val)
     row, col = pos
     grid[row][col] = val
   end
 
   def in_check?(color)
-    if color == :black
-      opponent = :white
-    else
-      opponent = :black
-    end
+    opponent = color == :black ? :white : :black
 
-    king = get_pieces(color).find {|piece| piece.is_a?(King)}
+    king = get_pieces(color).find { |piece| piece.is_a?(King) }
     get_pieces(opponent).each do |piece|
       return true if piece.moves.include?(king.position)
     end
@@ -55,11 +32,14 @@ class Board
     get_pieces(color).all? { |piece| piece.valid_moves.empty? }
   end
 
+  def get_pieces(color)
+    @grid.flatten.select { |piece| piece.color == color }
+  end
 
   def move_piece!(start_pos, end_pos)
-      self[start_pos], self[end_pos] = self[end_pos], self[start_pos]
-      self[end_pos].position = end_pos
-      remove_piece(start_pos) unless no_piece?(start_pos)
+    self[start_pos], self[end_pos] = self[end_pos], self[start_pos]
+    self[end_pos].position = end_pos
+    remove_piece(start_pos) unless no_piece?(start_pos)
   end
 
   def move_piece(start_pos, end_pos)
@@ -75,7 +55,6 @@ class Board
   end
 
   def remove_piece(pos)
-    attacked_piece = self[pos]
     self[pos] = NullPiece.instance
   end
 
@@ -87,7 +66,24 @@ class Board
     pos.all? { |coord| coord.between?(0, 7) }
   end
 
+  def dup
+    new_board = Board.new
+
+    self.grid.each_with_index do |row, i|
+      row.each_with_index do |piece, j|
+        if piece.is_a?(NullPiece)
+          new_board[[i, j]] = NullPiece.instance
+        else
+          new_board[[i, j]] = piece.class.new(piece.position, new_board, piece.color)
+        end
+      end
+    end
+
+    new_board
+  end
+
   protected
+
   def make_starting_grid
     grid = Array.new(8) { Array.new(8) }
 
@@ -95,18 +91,19 @@ class Board
     (0..7).each do |i|
       (0..7).each do |j|
         if i == 0
-          grid[i][j] = piece_order[j].new([i,j], self, :black)
+          grid[i][j] = piece_order[j].new([i, j], self, :black)
         elsif i == 1
-          grid[i][j] = Pawn.new([i,j], self, :black)
+          grid[i][j] = Pawn.new([i, j], self, :black)
         elsif i == 6
-          grid[i][j] = Pawn.new([i,j], self, :white)
+          grid[i][j] = Pawn.new([i, j], self, :white)
         elsif i == 7
-          grid[i][j] = piece_order[j].new([i,j], self, :white)
+          grid[i][j] = piece_order[j].new([i, j], self, :white)
         else
           grid[i][j] = NullPiece.instance
         end
       end
     end
+
     grid
   end
 end
