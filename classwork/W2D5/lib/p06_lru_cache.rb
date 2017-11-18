b0d0b44d0477b7f864e77a2e8c9572afc6bf5a29
agarun <19801205+agarun@users.noreply.github.com>
2017-11-17 20:35:56 -0500

    Remove redundant `elsif` block

--- classwork/W2D5/lib/p06_lru_cache.rb
+++ classwork/W2D5/lib/p06_lru_cache.rb
@@ -3,7 +3,6 @@ require_relative 'p04_linked_list'
 require "byebug"
 
 class LRUCache
-  attr_reader :count
   def initialize(max, prc = Proc.new { |x| x**2 })
     @map = HashMap.new
     @store = LinkedList.new
@@ -19,14 +18,10 @@ class LRUCache
     if @map.include?(key)
       node = @map.get(key)
       update_node!(node)
-    elsif count < @max
+    else
+      eject! if count >= @max
       val = calc!(key)
       @store.append(key, val)
-      @map.set(key, @store.last)
-    else
-      val = calc!(key)
-      eject!
-      @store.append(key,val)
       @map.set(key, @store.last)
     end
 
