d9a0331b3a01026e65309d522ca98d75d6b0d0f1
agarun <19801205+agarun@users.noreply.github.com>
2017-11-19 11:01:26 -0500

    Update Play

---
+++ homeworks/W3D2/playdbconnection.rb
@@ -0,0 +1,12 @@
+require 'sqlite3'
+require 'singleton'
+
+class PlayDBConnection < SQLite3::Database
+  include Singleton
+
+  def initialize
+    super('plays.db')
+    self.type_translation = true
+    self.results_as_hash = true
+  end
+end
