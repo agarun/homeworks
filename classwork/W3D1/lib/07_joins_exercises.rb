696fd58fa80047b328acd18db62fb014ad0908e3
agarun <19801205+agarun@users.noreply.github.com>
2017-11-20 20:39:52 -0500

    Add SQLZoo 5-9

--- classwork/W3D1/lib/07_joins_exercises.rb
+++ classwork/W3D1/lib/07_joins_exercises.rb
@@ -40,6 +40,16 @@ end
 def ford_films
   # List the films in which 'Harrison Ford' has appeared.
   execute(<<-SQL)
+  SELECT
+    movies.title
+  FROM
+    movies
+  JOIN
+    castings ON movies.id = castings.movie_id
+  JOIN
+    actors ON castings.actor_id = actors.id
+  WHERE
+    actors.name = 'Harrison Ford'
   SQL
 end
 
@@ -48,12 +58,34 @@ def ford_supporting_films
   # role. [Note: the ord field of casting gives the position of the actor. If
   # ord=1 then this actor is in the starring role]
   execute(<<-SQL)
+  SELECT
+    movies.title
+  FROM
+    movies
+  JOIN
+    castings ON movies.id = castings.movie_id
+  JOIN
+    actors ON castings.actor_id = actors.id
+  WHERE
+    actors.name = 'Harrison Ford' AND
+    castings.ord != 1
   SQL
 end
 
 def films_and_stars_from_sixty_two
   # List the title and leading star of every 1962 film.
   execute(<<-SQL)
+    SELECT
+      movies.title, actors.name
+    FROM
+      movies
+    JOIN
+      castings ON movies.id = castings.movie_id
+    JOIN
+      actors ON castings.actor_id = actors.id
+    WHERE
+      movies.yr = 1962 AND
+      castings.ord = 1
   SQL
 end
 
@@ -61,13 +93,53 @@ def travoltas_busiest_years
   # Which were the busiest years for 'John Travolta'? Show the year and the
   # number of movies he made for any year in which he made at least 2 movies.
   execute(<<-SQL)
+  SELECT
+    movies.yr,
+    COUNT(movies.yr) AS count
+  FROM
+    movies
+  JOIN
+    castings ON movies.id = castings.movie_id
+  JOIN
+    actors ON castings.actor_id = actors.id
+  WHERE
+    actors.name = 'John Travolta'
+  GROUP BY
+    movies.yr
+  HAVING
+    COUNT(movies.yr) >= 2
   SQL
 end
 
 def andrews_films_and_leads
   # List the film title and the leading actor for all of the films 'Julie
   # Andrews' played in.
+
+  # Pick the leading actor in movies where Julie andrews has starred
   execute(<<-SQL)
+  SELECT
+    movies.title,
+    actors.name
+  FROM
+    movies
+  JOIN
+    castings ON movies.id = castings.movie_id
+  JOIN
+    actors ON castings.actor_id = actors.id
+  WHERE
+    castings.ord = 1 AND
+    movies.title IN (
+      SELECT
+        movies.title
+      FROM
+        movies
+      JOIN
+        castings ON movies.id = castings.movie_id
+      JOIN
+        actors ON castings.actor_id = actors.id
+      WHERE
+        actors.name = 'Julie Andrews'
+    )
   SQL
 end
 
@@ -75,6 +147,21 @@ def prolific_actors
   # Obtain a list in alphabetical order of actors who've had at least 15
   # starring roles.
   execute(<<-SQL)
+    SELECT
+      actors.name
+    FROM
+      actors
+    JOIN
+      castings ON castings.actor_id = actors.id
+    WHERE
+      castings.ord = 1
+    GROUP BY
+      actors.name, castings.ord
+    HAVING
+      COUNT(actors.name) >= 15
+    ORDER BY
+      actors.name
+    -- count the number of times they've had `ord = 1`
   SQL
 end
 
@@ -82,11 +169,48 @@ def films_by_cast_size
   # List the films released in the year 1978 ordered by the number of actors
   # in the cast (descending), then by title (ascending).
   execute(<<-SQL)
+    SELECT
+      movies.title,
+      COUNT(actors.id)
+    FROM
+      movies
+    JOIN
+      castings ON movies.id = castings.movie_id
+    JOIN
+      actors ON castings.actor_id = actors.id
+    WHERE
+      movies.yr = 1978
+    GROUP BY
+      movies.title
+    ORDER BY
+      COUNT(actors.id) DESC,
+      movies.title ASC
   SQL
 end
 
 def colleagues_of_garfunkel
   # List all the people who have played alongside 'Art Garfunkel'.
   execute(<<-SQL)
+    SELECT
+      actors.name
+    FROM
+      actors
+    JOIN
+      castings ON actors.id = castings.actor_id
+    JOIN
+      movies ON movies.id = castings.movie_id
+    WHERE
+      movies.title IN (
+        SELECT
+          movies.title
+        FROM
+          actors
+        JOIN
+          castings ON actors.id = castings.actor_id
+        JOIN
+          movies ON movies.id = castings.movie_id
+        WHERE
+          actors.name = 'Art Garfunkel'
+      ) AND actors.name != 'Art Garfunkel'
   SQL
 end
--- classwork/W3D1/lib/09_self_joins.rb
+++ classwork/W3D1/lib/09_self_joins.rb
@@ -17,18 +17,38 @@ require_relative './sqlzoo.rb'
 def num_stops
   # How many stops are in the database?
   execute(<<-SQL)
+    SELECT
+      COUNT(stops)
+    FROM
+      stops
   SQL
 end
 
 def craiglockhart_id
   # Find the id value for the stop 'Craiglockhart'.
   execute(<<-SQL)
+  SELECT
+    id
+  FROM
+    stops
+  WHERE
+    name = 'Craiglockhart'
   SQL
 end
 
 def lrt_stops
   # Give the id and the name for the stops on the '4' 'LRT' service.
   execute(<<-SQL)
+  SELECT
+    stops.id,
+    stops.name
+  FROM
+    stops
+  JOIN
+    routes ON stops.id = routes.stop_id
+  WHERE
+    routes.num = '4' AND
+    routes.company = 'LRT'
   SQL
 end
 
@@ -51,6 +71,19 @@ def connecting_routes
   # that link these stops have a count of 2. Add a HAVING clause to restrict
   # the output to these two routes.
   execute(<<-SQL)
+    SELECT
+      company,
+      num,
+      COUNT(*)
+    FROM
+      routes
+    WHERE
+      stop_id = 149 OR stop_id = 53
+    GROUP BY
+      company,
+      num
+    HAVING
+      COUNT(*) = 2
   SQL
 end
 
@@ -73,6 +106,17 @@ def cl_to_lr
   # Craiglockhart, without changing routes. Change the query so that it
   # shows the services from Craiglockhart to London Road.
   execute(<<-SQL)
+    SELECT
+      a.company,
+      a.num,
+      a.stop_id,
+      b.stop_id
+    FROM
+      routes a
+    JOIN
+      routes b ON a.company = b.company AND a.num = b.num
+    WHERE
+      a.stop_id = 53 AND b.stop_id = 149
   SQL
 end
 
@@ -100,6 +144,21 @@ def cl_to_lr_by_name
   # number. Change the query so that the services between 'Craiglockhart' and
   # 'London Road' are shown.
   execute(<<-SQL)
+    SELECT
+      a.company,
+      a.num,
+      stopa.name,
+      stopb.name
+    FROM
+      routes AS a
+    JOIN
+      routes AS b ON a.company = b.company AND a.num = b.num
+    JOIN
+      stops AS stopa ON a.stop_id = stopa.id
+    JOIN
+      stops AS stopb ON b.stop_id = stopb.id
+    WHERE
+      stopa.name = 'Craiglockhart' AND stopb.name = 'London Road'
   SQL
 end
 
@@ -107,6 +166,15 @@ def haymarket_and_leith
   # Give the company and num of the services that connect stops
   # 115 and 137 ('Haymarket' and 'Leith')
   execute(<<-SQL)
+    SELECT DISTINCT
+      a.company,
+      a.num
+    FROM
+      routes AS a
+    JOIN
+      routes AS b ON a.company = b.company AND a.num = b.num
+    WHERE
+      a.stop_id = 115 AND b.stop_id = 137
   SQL
 end
 
@@ -114,6 +182,19 @@ def craiglockhart_and_tollcross
   # Give the company and num of the services that connect stops
   # 'Craiglockhart' and 'Tollcross'
   execute(<<-SQL)
+    SELECT DISTINCT
+      a.company,
+      a.num
+    FROM
+      routes AS a
+    JOIN
+      routes AS b ON a.company = b.company AND a.num = b.num
+    JOIN
+      stops AS stopa ON a.stop_id = stopa.id
+    JOIN
+      stops AS stopb ON b.stop_id = stopb.id
+    WHERE
+      stopa.name = 'Craiglockhart' AND stopb.name = 'Tollcross'
   SQL
 end
 
@@ -122,6 +203,20 @@ def start_at_craiglockhart
   # by taking one bus, including 'Craiglockhart' itself. Include the stop name,
   # as well as the company and bus no. of the relevant service.
   execute(<<-SQL)
+    SELECT DISTINCT
+      stopb.name,
+      a.company,
+      a.num
+    FROM
+      routes AS a
+    JOIN
+      routes AS b ON a.company = b.company AND a.num = b.num
+    JOIN
+      stops AS stopa ON a.stop_id = stopa.id
+    JOIN
+      stops AS stopb ON (b.stop_id = stopb.id)
+    WHERE
+      stopa.name = 'Craiglockhart'
   SQL
 end
 
@@ -130,5 +225,43 @@ def craiglockhart_to_sighthill
   # Sighthill. Show the bus no. and company for the first bus, the name of the
   # stop for the transfer, and the bus no. and company for the second bus.
   execute(<<-SQL)
+    SELECT
+      craig.num,
+      craig.company,
+      craig.name,
+      sight.num,
+      sight.company
+    FROM (
+      SELECT DISTINCT
+        stopb.name, -- stopb -> transfer
+        a.company,
+        a.num
+      FROM
+        routes AS a
+      JOIN
+        routes AS b ON a.company = b.company AND a.num = b.num
+      JOIN
+        stops AS stopa ON a.stop_id = stopa.id
+      JOIN
+        stops AS stopb ON b.stop_id = stopb.id
+      WHERE
+        stopa.name = 'Craiglockhart' -- stopa -> start
+      ) AS craig
+    JOIN (
+      SELECT DISTINCT
+        stopb.name, -- stopb -> transfer
+        a.company,
+        a.num
+      FROM
+        routes AS a
+      JOIN
+        routes AS b ON a.company = b.company AND a.num = b.num
+      JOIN
+        stops AS stopa ON a.stop_id = stopa.id
+      JOIN
+        stops AS stopb ON b.stop_id = stopb.id
+      WHERE
+        stopa.name = 'Sighthill' -- stopa -> end
+    ) AS sight ON craig.name = sight.name
   SQL
 end
