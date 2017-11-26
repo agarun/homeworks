3843a0ecd9d071979f435fba8272d5a2f377cbab
agarun <19801205+agarun@users.noreply.github.com>
2017-11-25 23:31:24 -0500

    Add books controller

--- homeworks/W4D1/controllers/app/controllers/books_controller.rb
+++ homeworks/W4D1/controllers/app/controllers/books_controller.rb
@@ -1,21 +1,28 @@
 class BooksController < ApplicationController
   def index
-    # your code here
+    @books = Book.all
+    render :index
   end
 
   def new
-    # your code here
+    render :new
   end
 
   def create
-    # your code here
+    Book.create(book_params)
+
+    index
   end
 
   def destroy
-    # your code here
+    book = Book.find(params["id"])
+    book.destroy
+
+    index
   end
 
   private
+
   def book_params
     params.require(:book).permit(:title, :author)
   end
