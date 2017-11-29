1f2759dc977628e351634b99c0d795f799555726
agarun <19801205+agarun@users.noreply.github.com>
2017-11-28 23:37:49 -0500

    Migrate indices

--- homeworks/W4D3/rails_auth/app/models/user.rb
+++ homeworks/W4D3/rails_auth/app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  validates :username, uniqueness: true
-  validates :username, :session_token, presence: true
+  before_validation :ensure_session_token
+  validates :username, :session_token, presence: true, uniqueness: true
   validates :password_digest, presence: { message: "Password can't be blank" }
-  validates :password, length: { minimum: 6 }, allow_nil: true
+  validates :password, length: { minimum: 6, allow_nil: true }
 
   def self.find_by_credentials(username, password)
     user = User.where(username: username).first
@@ -10,15 +10,15 @@ class User < ApplicationRecord
   end
 
   def self.generate_session_token
-    session[:session_token] ||= SecureRandom::urlsafe_base64
+    SecureRandom::urlsafe_base64(16)
   end
 
   def ensure_session_token
-    session[:session_token] ||= self.class.generate_session_token
+    self.session_token ||= self.class.generate_session_token
   end
 
   def reset_session_token!
-    session[:session_token] = nil
+    self.session_token = nil
     save!
   end
 
@@ -36,4 +36,5 @@ class User < ApplicationRecord
     # `self.password_digest` and `password` are strings, but re-instantiate
     # the digest as an object so BCrypt can read the metadata to build & compare digests
     BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
 end
