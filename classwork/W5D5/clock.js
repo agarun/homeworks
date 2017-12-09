939d30e59322f3ee3368b2714fbf647ebf92d96a
agarun <19801205+agarun@users.noreply.github.com>
2017-12-08 20:54:27 -0500

    Fix exercises

--- classwork/W5D5/clock.js
+++ classwork/W5D5/clock.js
@@ -3,19 +3,16 @@ class Clock {
     // 1. Create a Date object.
     this.date = new Date();
     // 2. Store the hours, minutes, and seconds.
-    // this.hours = this.date.getHours();
-    // this.minutes = this.date.getMinutes();
-    // this.seconds = this.date.getSeconds();
     this.hours = this.date.getHours();
-    this.minutes = 59;
-    this.seconds = 58;
+    this.minutes = this.date.getMinutes();
+    this.seconds = this.date.getSeconds();
     // 3. Call printTime.
     this.printTime();
-    // 4. Schedule the tick at 1 second intervals.  
+    // 4. Schedule the tick at 1 second intervals.
     // callback is invoked at 1 second intervals with `this` context
     // set to the clock instance
     setInterval(this._tick.bind(this), 1000);
-    // without `bind`, `this` refers to the global object on every 
+    // without `bind`, `this` refers to the global object on every
     // function-style invocation of the callback
     // with an arrow function as the callback, `bind` is unnecessary
     // because it shares the `this` from the constructor's scope
@@ -32,18 +29,18 @@ class Clock {
     // 2. Call printTime.
     this.date.setSeconds(this.seconds + 1);
     this.seconds = this.date.getSeconds();
-    
+
     if (this.seconds == 0) {
       this.date.setMinutes(this.minutes + 1);
       this.minutes = this.date.getMinutes();
-    } 
-    
+    }
+
     if (this.minutes == 0 && this.seconds == 0) {
       this.date.setHours(this.hours + 1);
       this.hours = this.date.getHours();
       this.minutes = 0;
     }
-    
+
     this.printTime();
   }
 }
--- classwork/W5D5/towersOfHanoi.js
+++ classwork/W5D5/towersOfHanoi.js
@@ -1,63 +1,58 @@
 class Game {
-  constructor(){
+  constructor() {
     this.stacks = [[3,2,1], [], []];
   }
-  
-  run(completionCallback){
+
+  run(completionCallback) {
     this.promptMove((startIndex, endIndex) => {
-      if (!this.move(startIndex, endIndex)){
+      if (!this.move(startIndex, endIndex)) {
         console.log("Move invalid");
       }
       if (!this.isWon()) {
         this.run(completionCallback);
       } else {
         console.log("You won!");
-        completionCallback();          
+        completionCallback();
       }
     });
   }
-  
+
   isWon() {
     return (this.stacks[0].length == 0 && (this.stacks[1].length == 0 || this.stacks[2].length == 0));
   }
-  
+
   promptMove(callback) {
     this.printStacks();
-    
-    reader.question("Enter from", function(from){
-      reader.question("Enter to", function(to){
+
+    reader.question("Enter from", function(from) {
+      reader.question("Enter to", function(to) {
         const startIndex = parseInt(from);
         const endIndex = parseInt(to);
         callback(startIndex, endIndex);
       });
-    });    
+    });
   }
-  
-  move(startIndex, endIndex){
-    if (this.isValidMove(startIndex, endIndex)){
+
+  move(startIndex, endIndex) {
+    if (this.isValidMove(startIndex, endIndex)) {
       const val = this.stacks[startIndex].pop();
       this.stacks[endIndex].push(val);
       return true;
     }
     return false;
   }
-  
-  isValidMove(start, end){
-    if(start < 0 || start > 2 || end < 0 || end > 2){
-      return false;
-    }
-    // start is empty, so can't select
-    if(this.stacks[start].length == 0){
-      return false;
-    }
-    if(this.stacks[end][this.stacks.length-1] < this.stacks[start][this.stacks.length-1]){
+
+  isValidMove(start, end) {
+    if (start < 0 || start > 2 || end < 0 || end > 2) return false;
+    if (this.stacks[start].length === 0) return false;
+    if (this.stacks[end][this.stacks.length - 1] < this.stacks[start][this.stacks.length - 1]) {
       return false;
     }
     return true;
   }
-  
-  printStacks(){
-    for (var i = 0; i < this.stacks.length; i++) {
+
+  printStacks() {
+    for (let i = 0; i < this.stacks.length; i += 1) {
       console.log(`${i}: ${JSON.stringify(this.stacks[i])}`);
     }
     console.log("---------");
